d5a8b03ecfbb24eeea78c6acc62770eb19593d74
Universe <dnwn138@gmail.com>
2022-07-21 10:30:02 +0900

    ChampName - 아무무 추가

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import requests
 
 url='http://ddragon.leagueoflegends.com/cdn/12.13.1/data/en_US/champion/Aatrox.json'
 url2='http://ddragon.leagueoflegends.com/cdn/12.13.1/data/en_US/champion/Ahri.json'
+url3='http://ddragon.leagueoflegends.com/cdn/12.13.1/data/en_US/champion/Amumu.json'
+
 
 res=requests.get(url).json()
-res2=requests.get(url).json()
+res2=requests.get(url2).json()
+res3=requests.get(url3).json()
 
 champ_lst=(res['data']['Aatrox']['key'], res['data']['Aatrox']['name'])
 champ_lst2=(res['data']['Ahri']['key'], res['data']['Ahri']['name'])
+champ_lst3=(res['data']['Ahri']['key'], res['data']['Amumu']['name'])
